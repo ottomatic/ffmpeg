01b73ae2126af874c7d63179c9db1e50c86ffebc
Otto Dandenell <otto@fetaste.com>
2013-01-30 02:19:25 +0100

    Bootstrap data parsing pattern works, needs to be completed

--- libavformat/hdsdec.c
+++ libavformat/hdsdec.c
@@ -45,35 +45,46 @@
 #else
 #define XML_FMT_INT_MOD "l"
 #endif
-static unsigned int hds_read_int32(const uint8_t *s, int pos);
-static uint64_t hds_read_int64(const uint8_t (*s), int pos);
 
-struct content_block {
+typedef struct ContentBlock {
     char *str;      ///< zero terminated string of read data in the current chunk
     int len;        ///< length of str, not accounting the '\0' end character
     uint8_t *dec;   ///< the decoded version of str (base64 or just stripped string), zero terminated
     int dec_len;    ///< length of dec, not accounting the '\0' end character
-};
+    int dec_ptr;    ///< pointer to where in the buffer the parsing of the content block is happening.
+} ContentBlock;
+/*
+ * @name functions for reading data from base64 decoded blocks
+ * in medias or bootstraps
+ * */
+static unsigned int hds_read_unsigned_byte(ContentBlock *block);
+static unsigned int hds_read_int16(ContentBlock *block);
+static unsigned int hds_read_int24(ContentBlock *block);
+static unsigned int hds_read_int32(ContentBlock *block);
+static uint64_t hds_read_int64(ContentBlock *block);
 
 struct media {
     int bootstrap_id; // 0-based index of the bootstrap associated with the media
     //char url[MAX_URL_SIZE];
     AVFormatContext *ctx;
     AVPacket pkt;
-    //AVIOContext pb;
+    AVIOContext pb;
+    AVFormatContext *parent;
 
     /* from xml */
     char *stream_id;
     char *url;
     int bitrate;
     char *bootstrap_info_id;
-    struct content_block metadata;
+    ContentBlock metadata;
 };
 
 typedef struct Bootstrap {
     u_int8_t version; ///< Either 0 or 1.
 
-    unsigned int bootstrap_info_version; ///< The version number of the boostrap information.
+    u_int8_t flags; ///< Reserved. Set to 0.
+
+    unsigned int bootstrap_version; ///< The version number of the boostrap information.
                                          /// When the update field is set, bootstrap_info_version
                                          /// indicates the version number that is being updated.
 
@@ -93,7 +104,7 @@ typedef struct Bootstrap {
                                      ///  converted to milliseconds. This ioffset is NOT in time_scale units.
                                      /// The field is zero when not used.
 
-    char *movie_identifier; ///< The identifier of this presentation. It can be a filename or a url.
+    char movie_identifier[MAX_URL_SIZE]; ///< The identifier of this presentation. It can be a filename or a url.
     
     int nb_server_entries; ///< The number of server entries. The minimum value is 0.
     
@@ -125,7 +136,7 @@ typedef struct BootstrapInfo {
     /* from xml */
     char *id;
     char *profile;
-    struct content_block data;
+    ContentBlock data;
     
     Bootstrap bootstrap;
 } BootstrapInfo;
@@ -143,9 +154,9 @@ typedef struct HDSContext {
     AVIOInterruptCB *interrupt_callback;
 
     /* xml info */
-    struct content_block id;            ///< "id" root field
-    struct content_block stream_type;   ///< "streamType" root field
-    struct content_block duration;      ///< "duration" float root field
+    ContentBlock id;            ///< "id" root field
+    ContentBlock stream_type;   ///< "streamType" root field
+    ContentBlock duration;      ///< "duration" float root field
 
     struct media **medias;              ///< array of media
     int nb_medias;                      ///< number of element in the medias array
@@ -178,18 +189,22 @@ static void XMLCALL xml_start(void *data, const char *el, const char **attr)
     int i;
     HDSContext *hds = data;
 
+    av_log(NULL,AV_LOG_DEBUG,"  In xml_start \n");
+
     hds->data_type = DATA_TYPE_NONE;
     if      (!strcmp(el, "id"))         hds->data_type = DATA_TYPE_ID;
     else if (!strcmp(el, "streamType")) hds->data_type = DATA_TYPE_STREAM_TYPE;
     else if (!strcmp(el, "duration"))   hds->data_type = DATA_TYPE_DURATION;
     else if (!strcmp(el, "media")) {
+        av_log(NULL,AV_LOG_DEBUG,"  data_type is media. Allocating media struct. \n");
         struct media *m = av_mallocz(sizeof(*m));
         if (!m) {
             hds->parse_ret = AVERROR(ENOMEM);
             return;
         }
+        av_log(NULL,AV_LOG_DEBUG,"  adding media to dynarray. \n");
         dynarray_add(&hds->medias, &hds->nb_medias, m);
-
+ 
         for (i = 0; attr[i]; i += 2) {
             if      (!strcmp(attr[i], "streamId"))        m->stream_id         = av_strdup(attr[i + 1]);
             else if (!strcmp(attr[i], "url"))             m->url               = av_strdup(attr[i + 1]);
@@ -199,11 +214,14 @@ static void XMLCALL xml_start(void *data, const char *el, const char **attr)
     } else if (!strcmp(el, "metadata")) {
         hds->data_type = DATA_TYPE_MEDIA_METADATA;
     } else if (!strcmp(el, "bootstrapInfo")) {
+        av_log(NULL,AV_LOG_DEBUG,"  data_type is bootstrapInfo. Allocating BootstrapInfo type. \n");
+        
         BootstrapInfo *bi = av_mallocz(sizeof(*bi));
         if (!bi) {
             hds->parse_ret = AVERROR(ENOMEM);
             return;
-        }
+        }     
+        av_log(NULL,AV_LOG_DEBUG,"  adding BootstrapInfo to dynarray. \n");
         dynarray_add(&hds->bs_info, &hds->nb_bs_info, bi);
 
         for (i = 0; attr[i]; i += 2) {
@@ -214,7 +232,7 @@ static void XMLCALL xml_start(void *data, const char *el, const char **attr)
     }
 }
 
-static struct content_block *get_content_block(HDSContext *hds, int data_type)
+static ContentBlock *get_content_block(HDSContext *hds, int data_type)
 {
     switch (hds->data_type) {
     case DATA_TYPE_ID:              return &hds->id;
@@ -229,7 +247,8 @@ static struct content_block *get_content_block(HDSContext *hds, int data_type)
 static void XMLCALL xml_data(void *data, const char *s, int len)
 {
     HDSContext *hds = data;
-    struct content_block *block = get_content_block(hds, hds->data_type);
+    ContentBlock *block = get_content_block(hds, hds->data_type);
+    av_log(NULL,AV_LOG_DEBUG,"  In xml_data \n");
 
     if (!block || !len || block->len >= INT_MAX - len - 1)
         return;
@@ -246,28 +265,45 @@ static void XMLCALL xml_data(void *data, const char *s, int len)
 static void XMLCALL xml_end(void *data, const char *el)
 {
     HDSContext *hds = data;
-    struct content_block *block = get_content_block(hds, hds->data_type);
+    ContentBlock *block = get_content_block(hds, hds->data_type);
+
+    av_log(NULL,AV_LOG_DEBUG,"  In xml_end \n");
     /* block can be null if we have a node which we don't track, such as akamai:version */
     if (!block)
         return;
+ 
+         
     if (block->len && !block->dec) {
-        /* [rl]strip the string */
         int i;
+
+        av_log(NULL,AV_LOG_DEBUG,"  setting zero lengths. \n");
+        block->dec_len = 0;
+        block->dec_ptr = 0;
+        av_log(NULL,AV_LOG_DEBUG,"  zero lengths were set. \n");
+
+        av_log(NULL,AV_LOG_DEBUG,"  rl-stripping string. \n");
+        /* [rl]strip the string */
         char *s = block->str;
         for (i = block->len - 1; i >= 0 && isspace(s[i]); i--)
             s[i] = 0;
         while (isspace(*s))
             s++;
 
+        av_log(NULL,AV_LOG_DEBUG,"  checking data type. \n");
         if (hds->data_type == DATA_TYPE_MEDIA_METADATA ||
             hds->data_type == DATA_TYPE_BOOTSTRAP_INFO) {
+            av_log(NULL,AV_LOG_DEBUG,"  This is media or bootstrap. Allocating decoding buffer to block->len size (%d). \n", block->len);
             /* decode base64 fields */
-            block->dec = av_malloc(block->len);
+            block->dec = av_mallocz(block->len);
             if (!block->dec) {
+                av_log(NULL,AV_LOG_DEBUG,"  decoding buffer was NOT allocated. Setting hds->parse_ret\n");
                 hds->parse_ret = AVERROR(ENOMEM);
                 return;
             }
+            av_log(NULL,AV_LOG_DEBUG,"  decoding buffer was allocated. \n");
+            av_log(NULL,AV_LOG_DEBUG,"  decoding, [%d] characters. \n", block->len);
             block->dec_len = av_base64_decode(block->dec, s, block->len);
+            av_log(NULL,AV_LOG_DEBUG,"  block->dec_len: [%d]. \n", block->dec_len);
         } else if (hds->data_type == DATA_TYPE_ID ||
                    hds->data_type == DATA_TYPE_STREAM_TYPE ||
                    hds->data_type == DATA_TYPE_DURATION) {
@@ -285,6 +321,8 @@ static int parse_manifest(AVFormatContext *s)
     char buf[4096];
     XML_Parser xmlp;
     HDSContext *hds = s->priv_data;
+
+    av_log(s,AV_LOG_DEBUG,"  In parse_manifest \n");
 
     xmlp = XML_ParserCreate(NULL);
     if (!xmlp) {
@@ -330,56 +368,112 @@ static int parse_manifest(AVFormatContext *s)
 
 
 
-static int read_box_header(uint8_t *data, int *pos, char (*box_type)[4], uint64_t *box_size)
+static int read_box_header(ContentBlock *data, char (*box_type)[5], uint64_t *box_size)
 {
+    av_log(NULL,AV_LOG_DEBUG,"  In read_box_header \n");
+
+//    if (!data || !*data->dec)
+//        return -1;
     if (!data)
+    {
+        av_log(NULL,AV_LOG_DEBUG,"  In read_box_header, NO DATA!\n");
         return -1;
-    /* If there is no starting position we assume we shall start from the beginning */
-    if (pos == NULL)
-        *pos = 0;
+    }
+    av_log(NULL,AV_LOG_DEBUG,"  data->dec_len: [%d] \n", data->dec_len);
     
-    *box_size = (int64_t)hds_read_int32(data, *pos);
+    if (!data->dec)
+    {
+        av_log(NULL,AV_LOG_DEBUG,"  In read_box_header, NO data->dec!\n");
+        return -1;
+    }
+
+    av_log(NULL,AV_LOG_DEBUG,"  data->dec_len: [%d] \n", data->dec_len);
+    av_log(NULL,AV_LOG_DEBUG,"  data->dec_ptr: [%d] \n", data->dec_ptr);
+        
+    /* If there is no starting position we assume we shall start from the beginning */
+//    if (data->dec_ptr == NULL)
+//        data->dec_ptr = 0;
+    
+    av_log(NULL,AV_LOG_DEBUG,"  Calling hds_read_int32 \n");
+    *box_size = (int64_t)hds_read_int32(data);
     av_log(NULL,AV_LOG_DEBUG,"  box_size=[%"PRId64"] \n", *box_size);    
     
     /* The box type is in the next four bytes after the initial size */
-    strncpy(*box_type, data + 4, 4);
-    av_log(NULL,AV_LOG_DEBUG,"  box_type=[%s] \n", *box_type);        
+    strncpy(*box_type, data->dec + data->dec_ptr, 4);
+    *box_type[4] = '\0';
+    av_log(NULL,AV_LOG_DEBUG,"  determined box_type=[%s] \n", *box_type);        
 
+    data->dec_ptr += 4;
+    
     /* if the size is 1, it points to an extended size in the next 8 bytes */
     if (*box_size == 1)
     {
-        *box_size = hds_read_int64(data, pos + 8) - (uint64_t)16;
-        *pos += 16;
+        *box_size = hds_read_int64(data) - (uint64_t)16;
     }
     else
     {
         *box_size -= 8;
-        *pos += 8;
     }
     
     return 0;    
+}
+static int parse_bootstrap_box(BootstrapInfo *binfo)
+{
+    ContentBlock *data = &binfo->data;
+    
+    Bootstrap *bootstrap = &binfo->bootstrap; 
+    uint8_t temp;
+    
+    av_log(NULL,AV_LOG_DEBUG,"  In parse_bootstrap_box \n");
+
+    bootstrap->version = hds_read_unsigned_byte(data);
+    bootstrap->flags = hds_read_int24(data);
+    
+    bootstrap->bootstrap_version = hds_read_int32(data);
+    
+    temp = hds_read_unsigned_byte(data);
+    bootstrap->profile = (temp & 0xC0) >> 6;
+    bootstrap->live = ((temp & 0x20) == 0x20);
+    bootstrap->update = ((temp & 0x1) == 0x1);
+        
+    return 0;
 }
 
 static int parse_bootstrap_data(BootstrapInfo *binfo)
 {
     int ret = 0;
-    int pos = 0;
     uint64_t box_size = 0;
     
-    char box_type[4] = "";
-
-    uint8_t *dec = binfo->data.dec;
+    char box_type[5];
+    char expected_boxtype[5] = "abst";
+    
+    ContentBlock *data = &binfo->data;
   
     av_log(NULL,AV_LOG_DEBUG,"  In parse_bootstrap_data \n");
     
-    if (!dec)
+    if (!data)
         return -1;
+
+    av_log(NULL,AV_LOG_DEBUG,"  Calling read_box_header \n");
     
-    ret = read_box_header(dec, &pos, &box_type, &box_size);
+    ret = read_box_header(data, &box_type, &box_size);
+    if (ret < 0)
+        return ret;
+
+    av_log(NULL,AV_LOG_DEBUG,"  box_type: [%s], box_size: [%"PRId64"]\n", &box_type, box_size);
+    
+    if (!strcmp(box_type, expected_boxtype))
+    {
+        av_log(NULL,AV_LOG_ERROR,"  bootstrap meta data does not start whith bootstrap. box_type: [%s]\n", &box_type);        
+        return AVERROR_INVALIDDATA;
+    }
+
+    av_log(NULL,AV_LOG_DEBUG,"  ok. calling parse_bootstrap_box\n");        
+    ret = parse_bootstrap_box(binfo);
     if (ret < 0)
         return ret;
     
-    return 0;        
+    return 0;
 }
 
 static int hds_read_header(AVFormatContext *s)
@@ -402,11 +496,11 @@ static int hds_read_header(AVFormatContext *s)
     return 0;
 }
 
-static void destroy_block(struct content_block *block)
+static void destroy_block(ContentBlock *block)
 {
     av_freep(&block->str);
     av_freep(&block->dec);
-    block->len = block->dec_len = 0;
+    block->len = block->dec_len = block->dec_ptr = 0;
 }
 
 static int hds_read_close(AVFormatContext *s)
@@ -426,7 +520,7 @@ static int hds_read_close(AVFormatContext *s)
                m->stream_id, m->url, m->bitrate, m->bootstrap_info_id);
         av_log(NULL,AV_LOG_DEBUG,"  bootstrap_id=%d\n", m->bootstrap_id);
         av_log(NULL,AV_LOG_DEBUG,"  metadata (len=%d):\n", m->metadata.dec_len);
-        av_hex_dump_log(0,0, m->metadata.dec, m->metadata.dec_len);
+        av_hex_dump_log(0,0, (m->metadata).dec, m->metadata.dec_len);
     }
 
     for (i = 0; i < hds->nb_bs_info; i++) {
@@ -447,7 +541,8 @@ static int hds_read_close(AVFormatContext *s)
         av_freep(&hds->medias[i]->stream_id);
         av_freep(&hds->medias[i]->url);
         av_freep(&hds->medias[i]->bootstrap_info_id);
-        destroy_block(&hds->medias[i]->metadata);
+        //destroy_block(&hds->medias[i]->metadata);
+        av_freep(&hds->medias[i]->metadata);        
         av_freep(&hds->medias[i]);
     }
     av_freep(&hds->medias);
@@ -455,7 +550,8 @@ static int hds_read_close(AVFormatContext *s)
     for (i = 0; i < hds->nb_bs_info; i++) {
         av_freep(&hds->bs_info[i]->id);
         av_freep(&hds->bs_info[i]->profile);
-        destroy_block(&hds->bs_info[i]->data);
+        //destroy_block(&hds->bs_info[i]->data);
+        av_freep(&hds->bs_info[i]->data);
         av_freep(&hds->bs_info[i]);
         // todo: free booststrap
     }
@@ -486,39 +582,45 @@ AVInputFormat ff_hds_demuxer = {
 /* Functions to help parse Boxes as defined in the F4V / F4F spec 
  * Numbers in streams / base64 blocks are big endian.
  * */
-static unsigned int hds_read_unsigned_byte(const uint8_t *s, int pos)
+static unsigned int hds_read_unsigned_byte(ContentBlock *block)
 {
-    return s[pos];
+//    av_log(NULL,AV_LOG_DEBUG,"  block->dec_len=%d\n", block->dec_len);
+//    av_log(NULL,AV_LOG_DEBUG,"  block->len=%d\n", block->len);
+//    av_log(NULL,AV_LOG_DEBUG,"  block->dec_ptr=%d\n", block->dec_ptr);
+
+    if (block->dec_ptr < block->dec_len)
+        return block->dec[block->dec_ptr++];
+    return 0;
 }
 
-static unsigned int hds_read_int16(const uint8_t *s, int pos)
+static unsigned int hds_read_int16(ContentBlock *block)
 {
     unsigned int val;
-    val = hds_read_unsigned_byte(s, pos) << 8;
-    val |= hds_read_unsigned_byte(s, pos + 1);
+    val = hds_read_unsigned_byte(block) << 8;
+    val |= hds_read_unsigned_byte(block);
     return val;
 }
 
-static unsigned int hds_read_int24(const uint8_t *s, int pos)
+static unsigned int hds_read_int24(ContentBlock *block)
 {
     unsigned int val;
-    val = hds_read_int16(s, pos) << 8;
-    val |= hds_read_unsigned_byte(s, pos + 2);
+    val = hds_read_int16(block) << 8;
+    val |= hds_read_unsigned_byte(block);
     return val;
 }
 
-static unsigned int hds_read_int32(const uint8_t *s, int pos)
+static unsigned int hds_read_int32(ContentBlock *block)
 {
     unsigned int val;
-    val = hds_read_int16(s, pos) << 16;
-    val |= hds_read_int16(s, pos + 2);
+    val = hds_read_int16(block) << 16;
+    val |= hds_read_int16(block);
     return val;
 }
 
-static uint64_t hds_read_int64(const uint8_t *s, int pos)
+static uint64_t hds_read_int64(ContentBlock *block)
 {
     uint64_t val;
-    val = (uint64_t)hds_read_int32(s, pos) << 32;
-    val |= (uint64_t)hds_read_int32(s, pos + 4);
+    val = (uint64_t)hds_read_int32(block) << 32;
+    val |= (uint64_t)hds_read_int32(block);
     return val;
 }
